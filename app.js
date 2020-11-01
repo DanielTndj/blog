7ac54c5d679b29f03caad0424ccc2a9accca82b7
DanielTndj <daniel.tndj@gmail.com>
2020-11-01 11:05:53 +0700

    add path parameter to env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/.env" });
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${processces.env.DB_PASS}@cluster0.ihu46.mongodb.net/blogDB?retryWrites=true&w=majority`,

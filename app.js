83b7e64b6f001e4330f5f5b8c36647e5f547fdb1
DanielTndj <daniel.tndj@gmail.com>
2020-11-01 09:28:50 +0700

    Add CRUD

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect(
+  "mongodb://localhost/blogDB",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) =>
+    err ? console.log(err) : console.log("Succesfully running mongodb...")
+);
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please fill the title field"],
+  },
+  post: {
+    type: String,
+    required: [true, "Please fill the post field"],
+  },
+});
+
+const PostModel = mongoose.model("Post", postSchema);
+
+const homeStartingContent = "Welcome to my blog";
+const aboutContent = "My name is Daniel.";
+const contactContent = "Email: Daniel Tanudjaja";
 
 const app = express();
-const listPost = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    listPost: listPost,
+  PostModel.find({}, (err, result) => {
+    if (!err) {
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        listPost: result,
+      });
+    }
   });
 });
 
@@ -38,12 +57,12 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.title,
+  const post = new PostModel({
+    title: _.lowerCase(req.params.title),
     post: req.body.postBody,
-  };
+  });
 
-  listPost.push(post);
+  post.save();
 
   res.redirect("/");
 });
@@ -51,17 +70,15 @@ app.post("/compose", (req, res) => {
 app.get("/posts/:title", (req, res) => {
   const title = _.lowerCase(req.params.title);
 
-  listPost.forEach((el) => {
-    const postTitle = _.lowerCase(el.title);
-
-    if (title === postTitle) {
-      res.render("post", { post: el });
-    } else {
-      res.redirect("/");
+  PostModel.findOne({title: title}, (err, result)=>{
+    if(result){
+      res.render('post', {post: result})
+    }else{
+      res.redirect('/')
     }
-  });
+  })
 });
 
-app.listen(3000, function () {
+app.listen(3000 || process.env.PORT, function () {
   console.log("Server started on port 3000");
 });
